f66f9a2f4549097b3df600c2e4341c4babbf0318
unknown <karthikarthick1949@gmail.com>
2024-08-02 18:22:54 +0530

    tst

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
-function app(){
-    return <div><h1>Hello</h1></div>
+import React from 'react'
+import Message from "./Message/Message.jsx"
+
+//Fat Arrow Method
+let App=()=>{
+    return <div>
+        <h1>App Component</h1><hr />
+        <Message/>
+        <Message/>
+        <Message/>
+    </div>
 }
-export default app
+export default App
+
