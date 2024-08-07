82573a050543d03f37ff74530fbf23692cd60121
KARTHICK ALAGAR <karthikarthick1949@gmail.com>
2024-08-07 19:09:51 +0530

    tst

--- Backup/props/App.js
+++ Backup/props/App.js
@@ -1,9 +1,8 @@
-import User from "./Props Components/User"
-
+import CompA from './Props/CompA'
 let App = ()=>{
     return <div>
          <h1>App Component</h1><hr/> 
-         <User/>
+         <CompA/>
     </div>
 }
 export default App
